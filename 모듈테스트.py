1638e7d1def80cc966ff1801cd4f55994181ebcf
d_kyu <dkrara93@naver.com>
2023-06-01 12:21:28 +0900

    23.06.01

---
+++ 모듈테스트.py
@@ -0,0 +1,11 @@
+from mod1 import add, password
+from simple_colors import * 
+
+print(add(10, 20))
+print(password("http://naver.com"))
+
+
+print(green("hello"))
+print(green("hello", "bold"))
+print(green('hello', ['bold', 'underlined']))
+print(green("hello"), red("Python", "bold"))
